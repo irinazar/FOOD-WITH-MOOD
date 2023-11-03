c2af41fc9cdc8647c707499f71c7fff81b6fbb11
IRINA <user@MacBook-Air.local>
2023-11-03 10:44:08 +0300

    comment

--- client/src/components/Pages/OwnerAccount/OwnerAccount.tsx
+++ client/src/components/Pages/OwnerAccount/OwnerAccount.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AbsoluteCenter, Box, Divider } from '@chakra-ui/react';
 import RestorantInfo from './ui/RestorantInfo';
 import style from '../UserAccount/style.module.css';
 import Comment from './ui/Comment';
@@ -8,8 +9,18 @@ export default function OwnerAccount(): JSX.Element {
     <div className={style.container}>
       <RestorantInfo />
 
-      <div className={style['favorite-restaurants']}>Я владелец </div>
-      <div className={style['favorite-restaurants']}> Отзывы о моих ресторанах </div>
+      <Box position="relative" padding="10">
+        <Divider />
+        <AbsoluteCenter bg="gray.100" px="4" fontSize="xl" fontWeight="bold">
+          Я владелец
+        </AbsoluteCenter>
+      </Box>
+      <Box position="relative" padding="10">
+        <Divider />
+        <AbsoluteCenter bg="gray.100" px="4" fontSize="xl" fontWeight="bold">
+          Отзывы о моих ресторанах
+        </AbsoluteCenter>
+      </Box>
       <div className={style['comment-owner-container']}>
         <Comment />
       </div>
