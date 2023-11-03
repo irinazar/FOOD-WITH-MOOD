edf6c152786f6cba295de018dc7a0695423dca38
KLUNNYYY <klunnyyy@gmail.com>
2023-11-03 14:28:07 +0300

    country page editing

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({Comment, Favourite, Rating, Country, RestOwner}) {
+    static associate({Comment, Favourite, Rating, Country, RestOwner,Booking}) {
       this.hasMany(Comment, {foreignKey:'restaurantId'})
       this.hasMany(Favourite, {foreignKey:'restaurantId'})
       this.hasMany(Booking, {foreignKey:'restaurantId'})
