e4e0638ee53c391cf08f1e557f5a215c8416b157
KLUNNYYY <klunnyyy@gmail.com>
2023-11-08 18:33:20 +0300

    new seeds

--- client/src/components/UI/Animations/Parallax.tsx
+++ client/src/components/UI/Animations/Parallax.tsx
@@ -1,5 +1,5 @@
 import React, {  useRef } from 'react';
-import { motion, useInView, useAnimation, useScroll, useTransform } from 'framer-motion';
+import { motion,  useScroll, useTransform } from 'framer-motion';
 
 type Props = {
   children: JSX.Element;
